bcd87ee244106db0356e7bf74b66b5fdd7c04dfa
victor-pythia <victor@pythia.social>
2022-01-04 20:31:18 +0200

    x

--- frontend/pages/despre.vue
+++ frontend/pages/despre.vue
@@ -1,18 +1,5 @@
 <template>
   <div class="container">
-      <h1>Despre mine</h1>
-      <article>
-          <p>
-            In prezent sunt masterand la Facultatea de Matematica si Informatica - Artificial Inteligence.
-          </p>
-          <p>Lucrarea mea de licenta a constat in realizarea unui AI care nu doar ca a reusit sa ma bata la o versiune simplificata de sah (antichess),</p>
-          <p>dar a fost conceputa in asa fel incat orice utilizator sa poata intelege cum si de ce reteaua ajunge la mutarile pe care le alege, urmarind arborele de decizie.</p>
-          <p>Reteaua a invatat jucandu-se cu ea insasi, fara a avea explicatii suplimentare in afara de mutarile legale.</p>
-            <br>
-          <p>In paralel, impreuna cu 2 fizicieni de la Facultatea de Fizica de la Magurele, am pornit un start-up care isi propune sa</p>
-          <p>modeleze comportamentul social al oamenilor in spatiul fazelor, aducand computatiile anterior bazate pe forta bruta intr-un domeniu in care avem legi si formule concrete.</p>
-          <p>Avem rezultate asemanatoare cu modelele state-of-the-art, dar eficienta algoritmilor nostri este cu ordine de magnitudine mai mare decat in studiile facute pana acum.</p>
-      </article>
       <h1>Proiecte in educatie</h1>
       <article>
         <p>Recent, impreuna cu Universitatea din Reykjavik si cei doi fizicieni despre care am vorbit mai sus, Radu Tirca si Luca Mihailescu,</p>
@@ -41,7 +28,19 @@
       </ol>
       <p></p>
       <article>
-
+    <h1>Despre mine</h1>
+      <article>
+          <p>
+            In prezent sunt masterand la Facultatea de Matematica si Informatica - Artificial Inteligence.
+          </p>
+          <p>Lucrarea mea de licenta a constat in realizarea unui AI care nu doar ca a reusit sa ma bata la o versiune simplificata de sah (antichess),</p>
+          <p>dar a fost conceputa in asa fel incat orice utilizator sa poata intelege cum si de ce reteaua ajunge la mutarile pe care le alege, urmarind arborele de decizie.</p>
+          <p>Reteaua a invatat jucandu-se cu ea insasi, fara a avea explicatii suplimentare in afara de mutarile legale.</p>
+            <br>
+          <p>In paralel, impreuna cu 2 fizicieni de la Facultatea de Fizica de la Magurele, am pornit un start-up care isi propune sa</p>
+          <p>modeleze comportamentul social al oamenilor in spatiul fazelor, aducand computatiile anterior bazate pe forta bruta intr-un domeniu in care avem legi si formule concrete.</p>
+          <p>Avem rezultate asemanatoare cu modelele state-of-the-art, dar eficienta algoritmilor nostri este cu ordine de magnitudine mai mare decat in studiile facute pana acum.</p>
+      </article>
       </article>
   </div>
 </template>
